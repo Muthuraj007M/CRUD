03144172ecd5b5d99924fd39691995552588e788
Muthuraj M <muthuraj143m@gmail.com>
2023-11-13 16:58:52 +0530

    Add files via upload
    Using SpringDataJpa Postman

---
+++ springDataJPAPOSTMAN/src/main/java/com/example/demo/SpringDataJpapostmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpapostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpapostmanApplication.class, args);
+	}
+
+}
